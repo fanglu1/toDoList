75f1c4f29e49c65cbc61d3e4a22dee4ee838d204
fanglu1 <101123629+fanglu1@users.noreply.github.com>
2022-03-30 17:00:21 -0400

    make pretty

---
+++ toDoList.js
@@ -0,0 +1,20 @@
+// The text from the input box is cleared out.
+// When the user clicks on a list item, it is removed
+// Submit
+var txtVal = document.getElementById("txtVal").value;
+var listN = document.getElementById("list");
+
+function addLi() {
+  var liN = document.createElement("li");
+  var txtN = document.getElementById("txtVal");
+  liN.innerText = txtN.value;
+  listN.appendChild(liN);
+  // remove text after clicking
+  txtN.value = "";
+}
+
+listN.addEventListener('click', function(e){
+    var tdItem = e.target;
+    tdItem.remove();
+})
+
